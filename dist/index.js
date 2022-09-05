5ca57961a097f42941decc01fc01a6f75010dcd9
Adam Gardner <adga2012@gmail.com>
2022-09-05 15:52:16 -0600

    fix(index): remove dist/index.js

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./formik-strap.cjs.production.min.js')
-} else {
-  module.exports = require('./formik-strap.cjs.development.js')
-}
